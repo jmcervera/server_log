9921515aba05a6d451ef14e1202be9f58270977c
Juanma Cervera <juanmacervera@gmail.com>
2015-08-09 18:06:25 +0200

    basic parser class

---
+++ lib/parser.rb
@@ -0,0 +1,39 @@
+class Parser
+
+	# Inner class for holding page statistics
+	class PageStats
+		attr_reader :views, :addresses
+		def initialize
+			@views = 0
+			@addresses = []
+		end
+		def add_visit(ip_address)
+			@views += 1
+			@addresses << ip_address unless @addresses.include?(ip_address)
+		end
+	end
+
+	attr_reader :pages_visited
+
+	def initialize(path, line_parser)
+		raise ArgumentError unless File.exists?(path)
+		@log = File.open(path)
+		@line_parser = line_parser
+		@pages_visited = {}
+	end
+
+	def parse
+		@log.each_line do |line|
+			visit = @line_parser.parse(line)
+			register_visit(visit)
+		end
+	end
+
+	private
+	def register_visit(visit)
+		unless @pages_visited.has_key?(visit.page)
+			@pages_visited[visit.page] = PageStats.new
+		end
+		@pages_visited[visit.page].add_visit(visit.ip_address)
+	end
+end
---
+++ spec/lib/parser_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require "parser"
+
+RSpec.describe Parser do
+	let(:visit) { double("visit", page: '/about', ip_address: "127.0.0.0")}
+	let(:line_parser) { double("line parser", parse: visit) }
+
+	it "requires an existing file to parse" do
+		path = "non-existent"
+		allow(File).to receive(:exists?).with(path).and_return(false)
+		expect{ Parser.new(path, line_parser) }.to raise_exception(ArgumentError)
+	end
+
+	it "opens the file if existing" do
+		path = "webserver.log"
+		allow(File).to receive(:exists?).with(path).and_return(true)
+		expect(File).to receive(:open).with(path)
+		Parser.new(path, line_parser)
+	end
+
+	describe "#parse" do
+		let(:path) { "weserver.log "}
+		let(:opened_file) { double("file")}
+		let(:line) { "/about 127.0.0.0" }
+
+		before do
+			allow(File).to receive(:exists?).with(path).and_return(true)
+			allow(File).to receive(:open).with(path).and_return(opened_file)
+			allow(opened_file).to receive(:each_line).and_yield(line)
+			@parser = Parser.new(path, line_parser)
+		end
+
+		it "opens the file for reading" do
+			expect(opened_file).to receive(:each_line)
+			@parser.parse
+		end
+
+		describe "process each line" do
+			it "parses each line obtaining visit" do
+				expect(line_parser).to receive(:parse).with(line)
+				@parser.parse
+			end
+
+			it "register the visit" do
+				@parser.parse
+				pv = @parser.pages_visited
+				expect(pv.has_key?('/about')).to be_truthy
+				expect(pv['/about'].views).to eq(1)
+				expect(pv['/about'].addresses).to eq ['127.0.0.0']
+			end
+
+			it "increment number of views on second visit to the page" do
+				allow(opened_file).to receive(:each_line).and_yield(line).and_yield(line)
+				@parser.parse
+				pv = @parser.pages_visited
+				expect(pv['/about'].views).to eq(2)
+				expect(pv['/about'].addresses).to eq ['127.0.0.0']
+			end
+		end
+	end
+
+end
